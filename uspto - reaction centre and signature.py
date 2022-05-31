624ee3255f62d3959f0d84469b0077e3bf35dffb
Abhor <ic35450@imail.iitm.ac.in>
2022-05-31 15:46:59 +0530

    Added some multiprocessing functionalities to actions extractor script - buggy

--- uspto - reaction centre and signature.py
+++ uspto - reaction centre and signature.py
@@ -14,6 +14,7 @@ from rdkit.Chem import AllChem
 from rdkit.Chem import rdchem
 
 dataset = pd.read_csv("/home/abhor/Desktop/datasets/my_uspto/processed_data.csv", index_col=0)
+dataset = dataset.iloc[:500]
 
 # draw molecule with index
 def mol_with_atom_index( mol ):
@@ -132,35 +133,129 @@ class Reaction:
         # calc 
         return self.reactant.get_centre(), self.product.get_centre()
         
-rsig_list = []
-psig_list = []
-rcen_list = []
-pcen_list = []
 
-for i in range(dataset.shape[0]):
-    mol1 = Chem.MolFromSmiles(dataset["reactants"][i])
-    mol2 = Chem.MolFromSmiles(dataset["products"][i])
+def sig_and_cen_collector(df, return_dict = None):
+    temp_rsig_list = []
+    temp_psig_list = []
+    temp_rcen_list = []
+    temp_pcen_list = []
 
-    R = Reaction(mol1, mol2)
-    R.calculate_centres_and_signatures()
+    for i in range(df.shape[0]):
+        mol1 = Chem.MolFromSmiles(df["reactants"].iloc[i])
+        mol2 = Chem.MolFromSmiles(df["products"].iloc[i])
+
+        R = Reaction(mol1, mol2)
+        R.calculate_centres_and_signatures()
+        
+        rcen, pcen = R.get_centres()
+        rsig, psig = R.get_smiles_signatures()
+        
+        temp_rsig_list.append(rsig)
+        temp_psig_list.append(psig)    
+        temp_rcen_list.append(rcen)    
+        temp_pcen_list.append(pcen)    
+
+    if return_dict is not None:
+        return_dict["rsig"].extend(temp_rsig_list)
+        return_dict["psig"].extend(temp_psig_list)
+        return_dict["rcen"].extend(temp_rcen_list)
+        return_dict["pcen"].extend(temp_pcen_list)
+    else:
+        return temp_rsig_list, temp_psig_list, temp_rcen_list, temp_pcen_list
+
+#######################################
+# CALLING FUNCTIONS - MULTIPROCESSING #
+#######################################
+
+# multiprocess it - cuz some reactions go into infinite loops
+from multiprocessing.pool import ThreadPool
+from multiprocessing import Process, Manager
+import time
+
+def multiprocess_collector(df, return_dict):
+    '''
+    Collects signatures and centres for df using multiprocessing.
+    Due to some reactions taking too long, the multiprocessing happens in recursion - 100 -> 10 -> 1 (size of df to process)
+    Then all the results are collected and returned
+    '''
+    print(f"GOT DF OF SHAPE {df.shape}")
     
-    rcen, pcen = R.get_centres()
-    rsig, psig = R.get_smiles_signatures()
+    for x in ["rsig", "psig", "rcen", "pcen"]:
+        if x not in return_dict:
+            return_dict[x] = []
+    # spawn process to run on whole df
+    p = Process(target=sig_and_cen_collector, args=(df, return_dict))
+    p.start()
+    start_time = time.time()
+
+    # terminate if takes too long
+    done = False
+    kill_threshold = 10
+    while not done:
+        if p.is_alive():
+            if time.time() - start_time > kill_threshold:
+                p.kill()
+                break
+        else:
+            done = True
+    p.join()
+    print(return_dict)
+
+    # if completed successfully, return results
+    if done:
+        return return_dict["rsig"], return_dict["psig"], return_dict["rcen"], return_dict["pcen"]
+
+    # not done - if df of size 1, return defaults instead
+    if df.shape[0] == 1:
+        return_dict["rsig"].append('')
+        return_dict["psig"].append('')
+        return_dict["rcen"].append([])
+        return_dict["pcen"].append([])
+        return [''], [''], [[]], [[]]
     
-    rsig_list.append(rsig)
-    psig_list.append(psig)    
-    rcen_list.append(rcen)    
-    pcen_list.append(pcen)    
+    # not done - else divide df into 10 parts and repeat
+    elements = 100
+    if df.shape[0] % 10 != 0:  # add some to make divisible by 10
+        elements = df.shape[0]
+        while df.shape[0] % 10 != 0:
+            df = pd.concat([df, df.iloc[:100-df.shape[0]]])
     
-    print(i)
+    step_size = df.shape[0] // 10
+    for i in range(10):
+        multiprocess_collector(df.iloc[i*step_size: (i+1)*step_size], return_dict)
+    return return_dict["rsig"][:elements], return_dict["psig"][:elements], return_dict["rcen"][:elements], return_dict["pcen"][:elements]
 
-df = dataset.iloc[:100]
 
-df = df.drop("reagents", axis=1)
 
-df["rsig"] = rsig_list
-df["psig"] = psig_list
-df["rcen"] = rcen_list
-df["pcen"] = pcen_list
+if __name__ == "__main__":
+    # with ThreadPool(processes=1) as tp:
+    #     tp.map()
 
-df.to_csv("datasets/my_uspto/simulator_dataset.csv")
+    n = 100
+    manager = Manager()
+
+    rsig_list = []
+    psig_list = []
+    rcen_list = []
+    pcen_list = []
+
+
+    for i in range(dataset.shape[0]//n):
+        print(i*n, min(i*n+n, dataset.shape[0]))
+        man_dict = manager.dict()
+        a, b, c, d = multiprocess_collector(dataset.iloc[i*n:min(i*n+n, dataset.shape[0])], man_dict)
+        rsig_list.extend(a)
+        psig_list.extend(b)
+        rcen_list.extend(c)
+        pcen_list.extend(d)
+
+    print(rsig_list)
+
+    dataset = dataset.drop("reagents", axis=1)
+
+    dataset["rsig"] = rsig_list
+    dataset["psig"] = psig_list
+    dataset["rcen"] = rcen_list
+    dataset["pcen"] = pcen_list
+
+    dataset.to_csv("datasets/my_uspto/simulator_dataset.csv")
