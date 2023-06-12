0d63d40ad2dadcb02cd76df4a0329bffc423f874
Your Name <you@example.com>
2023-06-12 18:06:40 +0000

    Added the leftover py files (for safety)

---
+++ molclr_dataset.py
@@ -0,0 +1,66 @@
+import numpy as np
+
+import torch
+
+from rdkit import Chem
+from rdkit.Chem.rdchem import BondType as BT
+
+from torch_geometric.data import Data
+
+ATOM_LIST = list(range(1,119))
+CHIRALITY_LIST = [
+    Chem.rdchem.ChiralType.CHI_UNSPECIFIED,
+    Chem.rdchem.ChiralType.CHI_TETRAHEDRAL_CW,
+    Chem.rdchem.ChiralType.CHI_TETRAHEDRAL_CCW,
+    Chem.rdchem.ChiralType.CHI_OTHER
+]
+BOND_LIST = [
+    BT.SINGLE, 
+    BT.DOUBLE, 
+    BT.TRIPLE, 
+    BT.AROMATIC
+]
+BONDDIR_LIST = [
+    Chem.rdchem.BondDir.NONE,
+    Chem.rdchem.BondDir.ENDUPRIGHT,
+    Chem.rdchem.BondDir.ENDDOWNRIGHT
+]
+
+
+def molclr_mol_repr(smiles):
+    mol = Chem.MolFromSmiles(smiles)
+    mol = Chem.AddHs(mol)
+
+    N = mol.GetNumAtoms()
+    M = mol.GetNumBonds()
+
+    type_idx = []
+    chirality_idx = []
+    atomic_number = []
+    for atom in mol.GetAtoms():
+        type_idx.append(ATOM_LIST.index(atom.GetAtomicNum()))
+        chirality_idx.append(CHIRALITY_LIST.index(atom.GetChiralTag()))
+        atomic_number.append(atom.GetAtomicNum())
+
+    x1 = torch.tensor(type_idx, dtype=torch.long).view(-1,1)
+    x2 = torch.tensor(chirality_idx, dtype=torch.long).view(-1,1)
+    x = torch.cat([x1, x2], dim=-1)
+
+    row, col, edge_feat = [], [], []
+    for bond in mol.GetBonds():
+        start, end = bond.GetBeginAtomIdx(), bond.GetEndAtomIdx()
+        row += [start, end]
+        col += [end, start]
+        edge_feat.append([
+            BOND_LIST.index(bond.GetBondType()),
+            BONDDIR_LIST.index(bond.GetBondDir())
+        ])
+        edge_feat.append([
+            BOND_LIST.index(bond.GetBondType()),
+            BONDDIR_LIST.index(bond.GetBondDir())
+        ])
+
+    edge_index = torch.tensor([row, col], dtype=torch.long)
+    edge_attr = torch.tensor(np.array(edge_feat), dtype=torch.long)
+    data = Data(x=x, y=None, edge_index=edge_index, edge_attr=edge_attr)
+    return data
